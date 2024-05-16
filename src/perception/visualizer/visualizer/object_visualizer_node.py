03506172fd225d26e2720e8bd8d7464a1f81b143
david lim <127456413+dav1dddddddd@users.noreply.github.com>
2024-05-16 17:50:19 -0500

    Implemented diagnostics: Object Visualizer

--- src/perception/visualizer/visualizer/object_visualizer_node.py
+++ src/perception/visualizer/visualizer/object_visualizer_node.py
@@ -4,6 +4,11 @@ from navigator_msgs.msg import Object3D, Object3DArray
 from visualization_msgs.msg import Marker, MarkerArray
 from rclpy.duration import Duration
 
+import numpy as np
+from rosgraph_msgs.msg import Clock
+from diagnostic_msgs.msg import DiagnosticStatus, KeyValue
+from navigator_msgs.srv import RetrieveStatus
+
 # label to color mappings, RGB
 LABEL_TO_COLOR = {
     0: [1.0, 0.0, 0.0],     # Pedestrian
@@ -16,15 +21,63 @@ class ObjectVisualizerNode(Node):
     def __init__(self):
         super().__init__('object_visualizer_node')
 
+        self.statusBool = None
+        self.callback_count = -1
+        self.callback_arr = np.zeros((2,), dtype = int)
+
         self.subscription = self.create_subscription(
             msg_type = Object3DArray,
             topic = 'objdet3d_tracked',
             callback = self.visualize_objects,
             qos_profile = 1
         )
+        self.clock_sub = self.create_subscription(
+            msg_type = Clock,
+            topic = '/clock',
+            callback = self.clockCb,
+            qos_profile = 10
+        )
+
+        self.service = self.create_service(RetrieveStatus, 'retrieve_status', self.retrieveStatusCb)
 
         self.visualization_publisher = self.create_publisher(MarkerArray, 'objdet3d_viz', 10)
 
+    def clockCb(self, msg: Clock):
+        self.clock = msg.clock
+
+        self.callback_count += 1
+        self.callback_arr[self.callback_count] = 1
+
+    def retrieveStatusCb(self, response):
+        self.status = DiagnosticStatus()
+        self.stamp = KeyValue()
+
+        self.status.name = 'Object Visualizer'
+
+        self.stamp.key = 'stamp ID'
+        self.stamp.value = str(self.clock)
+
+        self.status.values.append(self.stamp)
+
+        for self.callback_count in self.callback_arr:
+            if self.callback_arr[self.callback_count] == 1:
+                self.statusBool = True
+            elif self.callback_arr[self.callback_count] == 0:
+                self.statusBool = False
+
+        if self.statusBool == True:
+            self.status.level = DiagnosticStatus.OK
+            self.status.message = 'Node is FUNCTIONING properly!'
+        elif self.statusBool == False:
+            self.status.level = DiagnosticStatus.ERROR
+            self.status.message = 'Node has encountered an ERROR!'
+        elif self.statusBool == None:
+            self.status.level = DiagnosticStatus.WARN
+            self.status.message = 'Status unknown...'
+
+        response.status = self.status
+        self.get_logger().info(f"Incoming request from Guardian to Object Visualizer...")
+        return response
 
     def visualize_objects(self, msg: Object3DArray):
         #self.get_logger().info(f"{msg.header}")
@@ -82,8 +135,9 @@ class ObjectVisualizerNode(Node):
             marker_array.markers.append(tag)
 
         self.visualization_publisher.publish(marker_array)
-        
 
+        self.callback_count += 1
+        self.callback_arr[self.callback_count] = 1
 
 def main(args=None):
     rclpy.init(args=args)
